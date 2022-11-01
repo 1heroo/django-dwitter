bba1f428c166ae902f1b1245a83a6fdd65db001e
Adil Shamuratov <iswearican_1@outlook.com>
2022-11-01 18:26:46 +0600

    f

---
+++ dwitter/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.contrib.auth.models import User, Group
+
+from .models import Profile
+
+
+class ItemInline(admin.StackedInline):
+    model = Profile
+    extra = 1
+
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = [ItemInline]
+    model = User
+    # Only display the "username" field
+    fields = ["username"]
+
+
+# admin.site.register(Profile)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
